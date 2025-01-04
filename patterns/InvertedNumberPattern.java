093574d4e4a7b6f69fefc684bd58e0af3c1ab315
bikash1376 <bikash13763@gmail.com>
2025-01-04 17:55:59 +0530

    recursion problems

--- patterns/InvertedNumberPattern.java
+++ patterns/InvertedNumberPattern.java
@@ -1,8 +1,8 @@
-public class InvertedRightAngledTriangle {
+public class InvertedNumberPattern {
     public static void main(String[] args) {
         for(int i=5;i>=1;i--) {
             for(int j=1;j<=i;j++) {
-                System.out.print("*");
+                System.out.print(j);
             }
             System.out.println();
         }
--- recursion/Recursion.java
+++ recursion/Recursion.java
@@ -1,22 +1,82 @@
-/*
 public class Recursion {
     public static void main(String[] args) {
+        int n = 10;
+
+        //System.out.println(printingReverse(n));
+        //printing(1,n);
+
+        //int sum = sum(n);
+        //System.out.println(sum);
+
+       //int fact = fact(n);
+        //System.out.println(fact);
+
+        //int x=3;
+        //System.out.println(exponent(x,n));
+
+        //System.out.println(fibonacci(n));
+
+        //int n1=200; int n2=160;
+        //System.out.println(gcd(n1,n2));
+
 
     }
-    public static int factorial() {
-
+    public static void printing(int i, int n) {
+        if (i > n) {
+            return;
+        }
+        else
+        {
+            System.out.println(i);
+            printing(i + 1, n);
+        }
     }
-    public static int fibonacci() {
 
+
+    public static int printingReverse(int n) {
+        if(n<=1)
+            return 1;
+        else {
+            System.out.println(n);
+            return printingReverse(n-1);
+        }
     }
-    public static int sumOfDigits() {
 
+    public static int sum(int n) {
+        if(n<1)
+            return 0;
+        else
+            return n + sum(n-1);
     }
-    public static int checkPalindrome() {
 
+    public static int fact(int n) {
+        if(n==0 || n==1)
+            return 1;
+        else
+            return n* fact(n-1);
     }
-    public static int sumOfElements() {
 
+
+    public static int exponent(int x, int n) {
+        if(n==0)
+            return 1;
+        else
+            return x* exponent(x,n-1);
+    }
+
+    public static int fibonacci(int n) {
+        if(n<=1)
+            return n;
+        else
+            return fibonacci(n-1) + fibonacci(n-2);
+    }
+
+    public static int gcd(int n1, int n2) {
+        if(n2!=0) {
+            return gcd(n2, n1%n2);
+        }
+        else {
+            return n1;
+        }
     }
 }
-*/
