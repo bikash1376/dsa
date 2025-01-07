47b1de0e0708d8ec6184fd95f9f5701e5e89b396
bikash1376 <bikash13763@gmail.com>
2025-01-07 20:37:17 +0530

    hashing problem solution

---
+++ hashing/hashing1.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class hashing1 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int[] arr = {1, 2, 2, 3, 1, 4};
+        System.out.println(frequencyCounting(arr, in));
+    }
+
+    public static int frequencyCounting(int[] arr, Scanner in) {
+        int[] hashArray = new int[10];
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] >= 0 && arr[i] < hashArray.length) {
+                hashArray[arr[i]] += 1;
+            }
+        }
+        System.out.println("Enter number of queries you want to search:");
+        int q = in.nextInt();
+        while (q-- > 0) {
+            int num = in.nextInt();
+            if (num >= 0 && num < hashArray.length) {
+                System.out.println("Frequency of " + num + " is: " + hashArray[num]);
+            } else {
+                System.out.println(num + " is out of range.");
+            }
+        }
+        return 0;
+    }
+}
